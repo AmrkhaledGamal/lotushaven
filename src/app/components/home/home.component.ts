cd446e44e06c9bb881d0720ed41676136fae8141
AmrkhaledGamal <amrkhaled45555@gmail.com>
2024-12-30 13:11:31 +0200

    second commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,96 @@
+import { CurrencyPipe, SlicePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Category, Products } from '../../core/interface/products';
+import { ProductService } from '../../core/service/product.service';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SlicePipe, CurrencyPipe, CarouselModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private _ProductService: ProductService) {}
 
+  products: Products[] = [];
+  categories: Category[] = [];
+  mainSlides = [
+    { src: 'slider-1.jpg', alt: 'slider-1' },
+    { src: 'slider-2.jpeg', alt: 'slider-2' },
+    { src: 'slider-3.jpeg', alt: 'slider-3' },
+  ];
+
+  ngOnInit() {
+    this._ProductService.getProducts().subscribe({
+      next: (response) => {
+        this.products = response.data;
+      },
+      error: (error) => {},
+    });
+
+    this._ProductService.getCategories().subscribe({
+      next: (response) => {
+        this.categories = response.data;
+      },
+      error: (error) => {},
+    });
+  }
+
+  categoryOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 1000,
+    navSpeed: 700,
+    navText: ['Back', 'next'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 5,
+      },
+    },
+    nav: false,
+  };
+
+  mainSlideOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 1000,
+    navSpeed: 700,
+    navText: ['Back', 'next'],
+    items: 1,
+    // responsive: {
+    //   0: {
+    //     items: 1,
+    //   },
+    //   400: {
+    //     items: 2,
+    //   },
+    //   740: {
+    //     items: 3,
+    //   },
+    //   940: {
+    //     items: 5,
+    //   },
+    // },
+    nav: false,
+  };
 }
--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -9,4 +9,22 @@ import { FooterComponent } from '../../components/footer/footer.component';
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  scrollUp: boolean = false;
+
+  constructor() {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 400) {
+        this.scrollUp = true;
+      } else {
+        this.scrollUp = false;
+      }
+    });
+  }
+
+  scrollTop(): void {
+    window.scroll({
+      top: 0,
+    });
+  }
+}
